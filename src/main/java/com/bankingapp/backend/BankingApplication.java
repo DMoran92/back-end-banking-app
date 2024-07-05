66feefe26241207918f8b3efa754ca94ba594db2
Dara MacMeanmain <dara.macmeanmain@gmail.com>
2024-07-05 19:38:03 +0100

    Update BankingApplication.java

--- src/main/java/com/bankingapp/backend/BankingApplication.java
+++ src/main/java/com/bankingapp/backend/BankingApplication.java
@@ -2,11 +2,12 @@ package com.bankingapp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class BankingApplication {
+public class BankingApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(BankingApplication.class, args);
     }
